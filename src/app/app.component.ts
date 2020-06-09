f0a86c21cfe96b3bffa39422760b497bb20ad5c9
banerjee-suchismita <suchismita.b07@gmail.com>
2020-06-09 20:12:52 +0530

    First commit
    First commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,22 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'angular-dashboard';
+
+  toggle1 = true;
+  toggle2 = false;
+  app1 = false;
+  app2 = false;
+  app3 = false;
+  app4 = false;
+  navbar1 = true;
+  navbar2 = false;
+  dell = true;
+  // Toggle(){
+  //   this.toggle1 = !this.toggle1;
+  //   this.toggle2 = !this.toggle2;
+  //   console.log("I am here?")
+  // }
+
+
+  
 }
